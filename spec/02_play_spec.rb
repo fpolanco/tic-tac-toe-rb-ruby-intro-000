4fb52ac2866db9d3dbadcdf563d5b530009e42f0
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-16 22:11:56 +0000

    Automatically backed up by Learn

--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -61,7 +61,7 @@ describe './lib/tic_tac_toe.rb' do
     end
 
     it 'checks if the game is draw after every turn' do
-      binding.binding.pry
+      binding.pry
       board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
       allow($stdout).to receive(:puts)
       allow(self).to receive(:gets).and_return("1", "2", "3")
