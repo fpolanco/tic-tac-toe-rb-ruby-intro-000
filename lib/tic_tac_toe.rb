834283cb583e7998648c07e001b5ee3d13132365
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-16 23:38:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -93,6 +93,7 @@ def won?(board)
   #  false
   if position_taken?(board,win_combination[0]) && board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]]
    return win_combination
+  end
  end
  false
 end
